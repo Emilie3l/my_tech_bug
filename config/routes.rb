ca8c4df9e6db4999fdf60f230a77532dfe0c7a83
Emilie Lockey <emilie.lockey@gmail.com>
2020-12-26 18:10:04 +0000

    Heroku db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :bulletins
   get 'appointments/create'
   get 'appointments/new'
   get 'appointments/show'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_020154) do
+ActiveRecord::Schema.define(version: 2020_12_26_180616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(version: 2020_11_25_020154) do
     t.string "category"
     t.string "topic"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bulletins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
